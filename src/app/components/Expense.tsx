cbf2b8aa6b5e1a5d241dae4e9816174766f854f0
Papish Limbu <lpapish97@gmail.com>
2024-09-24 00:28:24 +0545

    basic setup

---
+++ src/app/components/Expense.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Button } from "react-native";
+
+const Expense = () => {
+  return (
+    <View>
+      <Text>expense</Text>
+      <Button title="Click Me!" />
+    </View>
+  );
+};
+
+export default Expense;
---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import Expense from "./components/Expense";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+      <Expense />
+    </View>
+  );
+};
+
+export default index;
